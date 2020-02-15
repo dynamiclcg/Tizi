a1c68c08cf06126f24717c357c6c380b2aaecfad
LCG <635656078@qq.com>
2020-02-16 01:44:16 +0800

    【规范脚本】+【新增网络管理】 这个是两个一起合入的 (#4)
    * 重新规范下接口
    
    * 增加网络管理部分的命令

--- SafeCheck.sh
+++ SafeCheck.sh
@@ -6,65 +6,10 @@
 # 2、确认有暴力破解后需要进行相应的修改和处理
 # 确认处理完了之后可以进行其他操作
 
-# 设置一些常用的颜色用于后面输出
-COLOR_RED="\e[31m"
-COLOR_GRE="\e[32m"
-COLOR_CLEAN="\e[0m"
-COLOR_RED_FLUSH="\e[31;5m"
-COLOR_GRE_FLUSH="\e[32;5m"
+. AllCommon.sh
 
-
-
-if [ "x${EUID}" != "x0" ] ; then
-	showInfo 'e' "${COLOR_RED_FLUSH}当前脚本必须用root用户执行脚本！！！${COLOR_CLEAN}"
-	exit 1
-fi
-
-# Function : showInfo
-# Param :
-#    $1 type  t检查项，i正常输出，e错误数据
-#    $2 str     输出的数据
-# Return : NULL
-function showInfo()
-{
-    local type="$1"
-    local str="$2"
-    if [ "x${type}" == "x"  ] ; then
-	echo -e "${COLOR_RED_FLUSH}输出内容异常！${COLOR_CLEAN}"
-	return
-	fi
-    case ${type} in
-	't')
-	    # 输出检查项内容
-	    echo -e "========》检查项：${COLOR_GRE}${str}${COLOR_CLEAN}"
-	    ;;
-	'i')
-	    echo "[ info ] ${str}"
-	    ;;
-	'p')
-	    echo -e "检查结果：${COLOR_GRE}PASS${COLOR_CLEAN}"
-	    echo 
-	    ;;
-	'f')
-	    echo -e "检查结果：${COLOR_RED}FAIL${COLOR_CLEAN}"
-	    echo 
-	    ;;
-	'e')
-	    echo  "[ error ] ${str}"
-	    ;;
-	*)
-	    esac
-}
-
-# Simple test for show Function
-# function testShowInfo()
-# {
-#     showInfo t "lcg"
-#     showInfo i "info"
-#     showInfo e "wrong"
-#     showInfo p
-#     showInfo f
-# }
+# 先检查是否是root用户
+notRootExit
 
 # Function : checkBrute
 # Parm : NULL  不需要参数
